aa03884007335ada0d5f9954dc52b8600a3e3b23
DevGiack <devgiack@protonmail.com>
2023-02-15 09:11:23 +0100

    opacity change Logo

--- src/components/Home/Logo/index.jsx
+++ src/components/Home/Logo/index.jsx
@@ -1,53 +1,12 @@
-import './index.scss';
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
-import LogoJ from '../../../assets/images/logo-j.png';
+import LogoTitle from "../../../assets/images/logo-j.png"
+import './index.scss'
 
 const Logo = () => {
-        const bgRef = useRef()
-        const outlineLogoRef = useRef()
-        const solidLogoRef = useRef()
-      
-        useEffect(() => {
-          gsap.registerPlugin(DrawSVGPlugin)
-      
-          gsap
-            .timeline()
-            .to(bgRef.current, {
-              duration: 1,
-              opacity: 1,
-            })
-            .from(outlineLogoRef.current, {
-              drawSVG: 0,
-              duration: 20,
-            })
-      
-          gsap.fromTo(
-            solidLogoRef.current,
-            {
-              opacity: 0,
-            },
-            {
-              opacity: 1,
-              delay: 4,
-              duration: 4,
-            }
-          )
-        }, [])
-
-        return (
-                <div className="logo-container" ref={bgRef}>
-                <img
-                  className="solid-logo"
-                  ref={solidLogoRef}
-                  src={LogoJ}
-                  alt="J"
-                />
-          
-
-              </div>
-        )
+  return (
+    <div className="logo-container">
+      <img className="solid-logo" src={LogoTitle} />
+    </div>
+  )
 }
 
 export default Logo
--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import './index.scss'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import portfolioData from "../../data/portfolio.json"
+import portfolioData from '../../data/portfolio.json'
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -14,30 +14,28 @@ const Portfolio = () => {
   }, [])
 
   const renderPortfolio = (portfolio) => {
-        return (
-                <div className="images-container">
-                        {
-                                portfolio.map((port, idx) => {
-                                        return (
-                                                <div className='image-box' key={idx}>
-                                                        <img src={port.cover}  
-                                                        alt="portfolio" 
-                                                        className='portfolio-image' 
-                                                        />
-                                                        <div className='content'>
-                                                                <p className="title">{port.title}</p>
-                                                                <h4 className="description">{port.description}</h4>
-                                                                <button
-                                                                className='btn'
-                                                                onClick={() => window.open(port.url)}
-                                                                >Voir</button>
-                                                        </div>
-                                                </div>
-                                        )
-                                })
-                        }
-                </div>
-        )
+    return (
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={port.cover}
+                alt="portfolio"
+                className="portfolio-image"
+              />
+              <div className="content">
+                <p className="title">{port.title}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  Voir
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
   }
 
   return (
